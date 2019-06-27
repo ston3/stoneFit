9cc12aff00f886d200578468ac140d79f61e4d82
Rainer-Flores <rainer.flores@coxautoinc.com>
2019-06-27 16:43:26 -0400

    add nav bar

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,18 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <div>
+    <nav>
+      <li>
+        <ul>head</ul>
+        <ul>body</ul>
+        <ul>arms</ul>
+        <ul>legs</ul>
+      </li>
+    </nav>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum iure iusto tempore neque? Voluptatibus veritatis, voluptas officiis animi quis facere nesciunt magnam atque necessitatibus voluptatum eveniet, dolorem illum, aspernatur minima.
+    </p>
+  </div>
 </body>
 </html>
