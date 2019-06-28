e66a151c56bba24b3d7eba3b3ca817a5b4db665f
Rainer Flores Ibañez <rainer.flores64@gmail.com>
2019-06-28 15:32:28 -0400

    add html (#1)
    * add html
    
    * add app
    
    * another test
    
    * add nav bar

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+  <title>Document</title>
+</head>
+<body>
+  <div>
+    <nav>
+      <li>
+        <ul>head</ul>
+        <ul>body</ul>
+        <ul>arms</ul>
+        <ul>legs</ul>
+      </li>
+    </nav>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum iure iusto tempore neque? Voluptatibus veritatis, voluptas officiis animi quis facere nesciunt magnam atque necessitatibus voluptatum eveniet, dolorem illum, aspernatur minima.
+    </p>
+  </div>
+</body>
+</html>
